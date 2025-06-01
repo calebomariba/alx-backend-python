02db2b97674a6f51155211831367d9f5eb1bd152
calebomariba <calebomariba1993@gmail.com>
2025-06-01 19:56:35 +0300

    Add TestGithubOrgClient to test org property with mock

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Unit tests for client module.
+"""
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test class for GithubOrgClient.
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test GithubOrgClient.org returns correct value and calls get_json ones.
+        """
+        test_payload = {"name": org_name, "repos_url": f"https://api.github.com/orgs/{org_name}/repos"}
+        mock_get_json.return_value = test_payload
+        client = GithubOrgClient(org_name)
+        result = client.org
+        self.assertEqual(result, test_payload)
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
