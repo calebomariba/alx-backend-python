ee7c9f84e31031c1f66399805ac1ec1f085ff464
calebomariba <calebomariba1993@gmail.com>
2025-06-01 23:40:31 +0300

    testing

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
+from django.urls import routers.DefaultRouter()
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
