edc0e35043f1fc3536decf8a973638faa6d5d458
calebomariba <calebomariba1993@gmail.com>
2025-06-08 21:08:19 +0300

    Create a middleware that logs each user’s requests to a file

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'chats.middleware.RequestLoggingMiddleware',  # Add custom middleware
 ]
 
 AUTH_USER_MODEL = 'chats.User'
