bbbef12a44b76622d2224ff5bd933fe7df3cc689
calebomariba <calebomariba1993@gmail.com>
2025-06-15 21:41:26 +0300

    Use Signals for Deleting User-Related Data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
@@ -12,17 +13,25 @@ def create_message_notification(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
-    # Check if the instance exists in the database (i.e., it's an update)
     if instance.pk:
         try:
             old_message = Message.objects.get(pk=instance.pk)
-            # If content has changed, log the old content
             if old_message.content != instance.content:
                 MessageHistory.objects.create(
                     message=instance,
                     old_content=old_message.content,
-                    edited_by=instance.sender  # Assuming sender is the editor
+                    edited_by=instance.sender
                 )
-                instance.edited = True  # Mark the message as edited
+                instance.edited = True
         except Message.DoesNotExist:
-            pass  # Handle case where message doesn't exist yet
+            pass
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    # Delete messages where the user is the sender or receiver
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    # Delete notifications where the user is the recipient
+    Notification.objects.filter(user=instance).delete()
+    # Delete message history where the user is the editor
+    MessageHistory.objects.filter(edited_by=instance).delete()
